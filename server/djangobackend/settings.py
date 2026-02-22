e900306466a534e18113feeecf61d44241744032
elherhari <elherharimeryem@gmail.com>
2026-02-22 20:27:05 +0100

    Fix CSRF trusted origins for Railway deployment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -85,3 +85,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://car-dealership-fullstack-production.up.railway.app']
